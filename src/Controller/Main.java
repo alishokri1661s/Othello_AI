78baffbdcd0264e4b13be4578b169341e6bc919f
Ali Shokri <alishokri1661s@yahoo.com>
2021-01-15 15:09:09 +0330

    -nothing

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -7,7 +7,7 @@ import View.GUI;
 public class Main {
     public static void main(String[] args) {
         Board board =Board.getInstance();
-        GUI gui = GUI.getInstance();
+        GUI.getInstance();
         board.play(new Point(1,1));
     }
 }
